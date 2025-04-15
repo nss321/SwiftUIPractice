ea659ce835f23b4dad92500478912a00ad870cd3
BAE <lexon1209@gmail.com>
2025-04-15 23:34:13 +0900

    feat implement WalletView

--- SwiftUIPractice/View/WalletView.swift
+++ SwiftUIPractice/View/WalletView.swift
@@ -8,11 +8,88 @@
 import SwiftUI
 
 struct WalletView: View {
+    @State private var dummy = Money.mockData
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                ForEach(dummy, id: \.id) { item in
+                    NavigationLink {
+                        WalletItemDetailView(item: item)
+                            .padding(.horizontal, 40)
+                            .padding(.vertical, 200)
+                    } label: {
+                        WalletItem(item: item)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Sean's Wallet")
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+struct WalletItemDetailView: View {
+    let item: Money
+    var body: some View {
+        VStack(alignment: .trailing) {
+            HStack(alignment: .bottom) {
+                Text(item.product)
+                    .font(.title)
+                    .bold()
+                Spacer()
+                Text("\(item.category)")
+                    .foregroundStyle(.gray)
+                
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
+            
+            Divider()
+                .background(Color(uiColor: .label))
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, -10)
+            
+            Spacer()
+            Text(item.amountFormat)
+                .foregroundStyle(Color(uiColor: .label))
+                .padding()
+            
+        }
+        .frame(maxHeight: .infinity)
+        .padding()
+        .overlay {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color(uiColor: .label), lineWidth: 2)
+        }
+    }
+}
+
+struct WalletItem: View {
+    let item: Money
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.product)
+                    .font(.headline)
+                Text("\(item.category)")
+                    .font(.callout)
+                    .foregroundStyle(.gray)
+            }
+            Spacer()
+            Text(item.amountFormat)
+        }
+        .listRowSeparator(.hidden)
+    }
+    
+    init(item: Money) {
+        self.item = item
     }
 }
 
 #Preview {
-    WalletView()
+    NavigationView {
+        WalletView()
+    }
+//    WalletItem(item: Money(amount: 20000, product: "테스트", category: .deposit))
+//    WalletItemDetailView(item: Money(amount: 2000, product: "test", category: .food))
 }
